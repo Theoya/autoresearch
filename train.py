82ee1a25385c0826e1ca68c13541fa4b07fd3df0
Taylorx20 <taylorhadfield1@gmail.com>
2026-03-09 08:38:06 -0400

    Experiment: hidden_dim=64 with 30 seeds

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from prepare import (
 # Hyperparameters
 # ---------------------------------------------------------------------------
 
-HIDDEN_DIM = 48
+HIDDEN_DIM = 64
 DROPOUT = 0.5
 LR = 1e-3
 WEIGHT_DECAY = 0.01
